20f7f06ef60acef0150e4d5d3bd52d57abce8242
Emmanuel <emmanuelamon92@gmail.com>
2021-11-08 15:06:56 -0500

    added class names for home screen header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,23 @@ import './Home.css';
 function home() {
     return (
         <div className='home'>
-            
+            <h1>This is Homepage</h1>
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    {/* Link */}
+                    {/* Link */}
+                </div>
+                <div className='home__headerRight'>
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Icon */}
+                    {/* Avatar (Material UI) */}
+                </div>
+            </div>
+
+            <div className='home__body'>
+
+            </div>
         </div>
     )
 }
