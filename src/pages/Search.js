5fa8b54cbc93ae070a2f5d5129d0d9a0207cf21f
Emmanuel <emmanuelamon92@gmail.com>
2021-11-29 12:59:04 +0100

    made search component a form for submitting search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Search.css'
 import { Search as SearchIcon, Mic as MicIcon } from '@mui/icons-material';
 import { Button } from '@mui/material'
 
 
 function Search() {
+    const [input, setInput] = useState('')
+
+    const search = e => {
+        e.preventDefault();
+
+        console.log('you hit the search button >>', input)
+    }
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='search__input'>
                 <SearchIcon className='search__inputIcon' />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
 
             <div className='search__buttons'>
-                <Button varient='outlined'>Google Search</Button>
+                <Button type='submit' onClick={ search } varient='outlined'>Google Search</Button>
                 <Button varient='outlined'>I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
 
     )
 }
