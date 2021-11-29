0c2277ff2467b722a2187c3d6ab0d2f5a4141241
Emmanuel <emmanuelamon92@gmail.com>
2021-11-29 22:19:15 +0100

    added mock data with response.js file

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './SearchPage.css';
 import { useStateValue } from '../StateProvider';
 import useGoogleSearch from '../useGoogleSearch';
+import Response from '../response';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
-    const { data } = useGoogleSearch(term);
-
+    // LIVE API CALL
+    // const { data } = useGoogleSearch(term);
+    
+    // MOCK LIVE API CALL FROM response.js
+    const data = Response;
     //https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
 
     //https://cse.google.com/cse/create/new
