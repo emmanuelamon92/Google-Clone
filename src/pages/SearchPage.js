6f7b593c767348f8f1b8f40a1d842c28371d2ff5
Emmanuel <emmanuelamon92@gmail.com>
2021-11-30 14:17:37 +0100

    more styling of search page (spacing)

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -35,20 +35,20 @@ function SearchPage() {
                                 <Link to="/all">All</Link>
                             </div>
                             <div className="searchPage__option">
-                                <DescriptionIcon />
-                                <Link to="/news">News</Link>
-                            </div>
-                            <div className="searchPage__option">
                                 <ImageIcon />
                                 <Link to="/images">Images</Link>
                             </div>
                             <div className="searchPage__option">
-                                <LocalOfferIcon />
-                                <Link to="/shopping">Shopping</Link>
-                            </div>
-                            <div className="searchPage__option">
                                 <RoomIcon />
                                 <Link to="/maps">Maps</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">Shopping</Link>
                             </div>
                             <div className="searchPage__option">
                                 <MoreVertIcon />
