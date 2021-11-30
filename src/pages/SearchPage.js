442322cea03c1a24337bccde600957248177e107
Emmanuel <emmanuelamon92@gmail.com>
2021-11-30 13:15:07 +0100

    added logo and search bar to /search path

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './SearchPage.css';
+import { Link } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
-import useGoogleSearch from '../useGoogleSearch';
+// import useGoogleSearch from '../useGoogleSearch';
 import Response from '../response';
+import Search from './Search';
+import { Search as SearchIcon, Description as DescriptionIcon, Image as ImageIcon, LocalOffer as LocalOfferIcon, Room as RoomIcon, MoreVert as MoreVertIcon } from '@mui/material';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
@@ -19,10 +22,44 @@ function SearchPage() {
     return (
         <div className="searchPage">
             <div className="searchPage__header">
-                <h1>{term}</h1>
-            </div>
-            <div className="searchPage__results">
+                <Link to="/">
+                    <img className="searchPage__logo" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt=""/>
+                </Link>
             
+                <div className="searchPage__headerBody">
+                    <Search hideButtons />
+                    <div className="searchPage__options">
+                        <div className="searchPage__optionsLeft">
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images"></Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping"></Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps"></Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more"></Link>
+                            </div>
+                        </div>
+                        <div className="searchPage__optionsRight">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
